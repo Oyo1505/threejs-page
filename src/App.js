eac7924b5f2a697bd85928bb32c120d74d870497
0y0 <rigoulet.hneri.pierre@gmail.com>
2021-02-04 07:59:16 +0100

    setup scene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 2
-        </a>
-      </header>
-    </div>
+    <Canvas className="canvas" camera >
+      <color attach="background" args={['#131314']} ></color>
+    </Canvas>
   );
 }
 
